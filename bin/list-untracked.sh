b827739b71d11cb948d60f9c174eef0be25b9354
Manoj Srivastava <srivasta@golden-gryphon.com>
2014-06-16 14:58:10 -0700

    [master]: add the standard exclusion list to filter the untracked file list.
    Signed-off-by: Manoj Srivastava <srivasta@golden-gryphon.com>

--- bin/list-untracked.sh
+++ bin/list-untracked.sh
@@ -5,4 +5,4 @@ for i in $(vcsh list); do
     vcsh $i ls-files $HOME -z |
     GIT_INDEX_FILE=$tmp xargs -0 git update-index --add
 done
-GIT_INDEX_FILE=$tmp git ls-files -o
+GIT_INDEX_FILE=$tmp git ls-files -o --exclude-standard
